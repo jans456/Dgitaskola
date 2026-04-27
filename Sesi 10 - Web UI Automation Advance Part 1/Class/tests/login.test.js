7a1f38096259055060170134758603319c5ad082
jans456 <105582665+jans456@users.noreply.github.com>
2026-04-27 23:31:10 +0700

    update class marathon

--- Sesi 10 - Web UI Automation Advance Part 1/Class/tests/login.test.js
+++ Sesi 10 - Web UI Automation Advance Part 1/Class/tests/login.test.js
@@ -1,1 +1,19 @@
-const {Builder,}
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+describe('Login', () => {
+    it('should login with valid credentials', async () => {
+        const driver = new Builder().forBrowser('chrome').build();
+
+        await driver.get('https://www.saucedemo.com/');
+        await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+        await driver.findElement(By.id('password')).sendKeys('secret_sauce');
+        await driver.findElement(By.id('login-button')).click();
+
+        await driver.wait(until.elementLocated(By.className('title')), 5000);
+        const title = await driver.findElement(By.className('title')).getText();
+        assert.strictEqual(title, 'Products');
+
+        await driver.quit();
+    });
+});
